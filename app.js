7bb1aadc9a6a99c57f0a712e653fc2abe2a7c464
sa <samaradh1619@gmail.com>
2026-05-29 17:06:40 +1000

    updated

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("CI/CD is running");
+	res.send("CI/CD is running. I am running it. ");
 });
 
-app.listen(300, () => {
+app.listen(3000, "0.0.0.0", () => {
 console.log("server is running on port 3000");
 });
