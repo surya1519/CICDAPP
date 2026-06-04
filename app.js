c936bac622a3792f85c5b5f97c47fd510079e784
EC2 Default User <ec2-user@ip-172-31-4-36.ap-southeast-2.compute.internal>
2026-06-04 01:37:44 +0000

    change6

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("CI/CD");
+	res.send("this is CI/CD");
 });
 
 app.listen(3000, "0.0.0.0", () => {
