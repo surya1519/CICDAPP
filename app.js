ef058363005b84dff526f9706348d1050ac1b0df
EC2 Default User <ec2-user@ip-172-31-4-36.ap-southeast-2.compute.internal>
2026-06-04 07:30:29 +0000

    changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("this is CI/CD");
+	res.send("I am running in Container nowD");
 });
 
 app.listen(3000, "0.0.0.0", () => {
